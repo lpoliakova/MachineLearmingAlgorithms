415570e6a8825ceb00cd5bf4f62e42c158b07d3b
lpoliakova <olena.poliakova@gmail.com>
2017-12-17 22:37:57 +0200

    bayes algorithm without output

---
+++ src/main/java/algorithms/Bayes.java
@@ -0,0 +1,83 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Bayes {
+    private Map<String, Double> categoryProbabilities;
+    private Map<String, Map<String, Double>> wordInCategoryProbabilities;
+    private Integer uniqueAmount;
+
+    public void train(Map<String, String[]> texts) {
+        categoryProbabilities = trainCategoryProbability(texts);
+        wordInCategoryProbabilities = trainWordInCategoryProbability(texts);
+    }
+
+    public Map<String, Double> test(String text) {
+        String[] words = text.split(" ");
+        Map<String, Double> probabilities = new HashMap<>();
+        for (Map.Entry<String, Double> category : categoryProbabilities.entrySet()) {
+            Double probability = category.getValue();
+            for (String word : words) {
+                probability *= wordInCategoryProbabilities.get(category.getKey()).getOrDefault(word, 1.0 / uniqueAmount);
+            }
+            probabilities.put(category.getKey(), probability);
+        }
+        return probabilities;
+    }
+
+    Map<String, Double> trainCategoryProbability(Map<String, String[]> texts) {
+        int amountOfTexts = texts.values().stream().mapToInt(s -> s.length).sum();
+        return texts.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().length * 1.0 / amountOfTexts));
+    }
+
+    Map<String, Map<String, Double>> trainWordInCategoryProbability(Map<String, String[]> texts) {
+        Map<String, String[]> words = convertTextInWords(texts);
+
+        Map<String, Map<String, Integer>> wordsInCategory = new HashMap<>();
+        Map<String, Integer> wordsEverywhere = new HashMap<>();
+        for (Map.Entry<String, String[]> category : words.entrySet()) {
+            Map<String, Integer> wordsInCurrentCategory = new HashMap<>();
+            for (String word : category.getValue()) {
+                addWord(word, wordsInCurrentCategory);
+                addWord(word, wordsEverywhere);
+            }
+            wordsInCategory.put(category.getKey(), wordsInCurrentCategory);
+        }
+
+        uniqueAmount = wordsEverywhere.keySet().size();
+        Map<String, Map<String, Double>> result = new HashMap<>();
+        for (Map.Entry<String, Map<String, Integer>> category : wordsInCategory.entrySet()) {
+            Map<String, Double> categoryResult = new HashMap<>();
+            for (Map.Entry<String, Integer> word : wordsEverywhere.entrySet()) {
+                Integer wordInCategory = category.getValue().getOrDefault(word.getKey(), 0);
+                categoryResult.put(word.getKey(), (wordInCategory + 1) * 1.0 / (word.getValue() + uniqueAmount));
+            }
+            result.put(category.getKey(), categoryResult);
+        }
+        return result;
+    }
+
+    private Map<String, String[]> convertTextInWords(Map<String, String[]> texts) {
+        return texts.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> convertTextInWords(e.getValue())));
+    }
+
+    private String[] convertTextInWords(String[] texts) {
+        ArrayList<String> words = new ArrayList<>();
+        for (String text : texts) {
+            String[] wordsInText = text.split(" ");
+            words.addAll(Arrays.asList(wordsInText));
+        }
+        return words.toArray(new String[0]);
+    }
+
+    private void addWord(String word, Map<String, Integer> words) {
+        Integer count = words.getOrDefault(word, 0);
+        words.put(word, ++count);
+    }
+}
---
+++ src/test/java/algorithms/BayesTests.java
@@ -0,0 +1,144 @@
+package algorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BayesTests {
+    private final static double PRECISION = 0.000000000000000000000000001;
+    private final static String[] CATEGORIES = {"America", "Europe"};
+    private final static String[] WORDS = {"New", "York", "Mexico", "San", "Francisco", "Denver", "Washington", "Chicago", "Michigan",
+            "Seattle", "Orleans", "London", "Dover", "Castle", "Manchester", "Kyiv", "Brest", "Budapest"};
+
+    @Test
+    public void trainCategoryProbabilityTest() {
+        Map<String, String[]> texts = createTrainTexts();
+
+        Map<String, Double> categoryProbabilities = new HashMap<>();
+        categoryProbabilities.put(CATEGORIES[0], 5.0 / 9.0);
+        categoryProbabilities.put(CATEGORIES[1], 4.0 / 9.0);
+
+        Bayes bayes = new Bayes();
+        Map<String, Double> actual = bayes.trainCategoryProbability(texts);
+
+        Assert.assertEquals(categoryProbabilities.size(), actual.size());
+        for (String category : CATEGORIES) {
+            Assert.assertEquals(categoryProbabilities.get(category), actual.get(category), PRECISION);
+        }
+    }
+
+    @Test
+    public void trainWordInCategoryProbabilityTest() {
+        Map<String, String[]> texts = createTrainTexts();
+
+        Map<String, Double> americaWords = new HashMap<>();
+        americaWords.put(WORDS[0], (7 + 1) * 1.0 / (7 + 1 + WORDS.length));
+        americaWords.put(WORDS[1], (5 + 1) * 1.0 / (5 + 1 + WORDS.length));
+        americaWords.put(WORDS[2], (2 + 1) * 1.0 / (2 + WORDS.length));
+        americaWords.put(WORDS[3], (1 + 1) * 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[4], (1 + 1) * 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[5], (1 + 1) * 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[6], (1 + 1) * 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[7], (2 + 1) * 1.0 / (2 + WORDS.length));
+        americaWords.put(WORDS[8], (1 + 1) * 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[9], (1 + 1) * 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[10], 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[11], 1.0 / (4 + WORDS.length));
+        americaWords.put(WORDS[12], 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[13], 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[14], 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[15], 1.0 / (2 + WORDS.length));
+        americaWords.put(WORDS[16], 1.0 / (1 + WORDS.length));
+        americaWords.put(WORDS[17], 1.0 / (1 + WORDS.length));
+
+        Map<String, Double> europeWords = new HashMap<>();
+        europeWords.put(WORDS[0], (1 + 1) * 1.0 / (7 + 1 + WORDS.length));
+        europeWords.put(WORDS[1], (1 + 1) * 1.0 / (5 + 1 + WORDS.length));
+        europeWords.put(WORDS[2], 1.0 / (2 + WORDS.length));
+        europeWords.put(WORDS[3], 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[4], 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[5], 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[6], 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[7], 1.0 / (2 + WORDS.length));
+        europeWords.put(WORDS[8], 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[9], 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[10], (1 + 1) * 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[11], (4 + 1) * 1.0 / (4 + WORDS.length));
+        europeWords.put(WORDS[12], (1 + 1) * 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[13], (1 + 1) * 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[14], (1 + 1) * 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[15], (2 + 1) * 1.0 / (2 + WORDS.length));
+        europeWords.put(WORDS[16], (1 + 1) * 1.0 / (1 + WORDS.length));
+        europeWords.put(WORDS[17], (1 + 1) * 1.0 / (1 + WORDS.length));
+
+        Map<String, Map<String, Double>> wordInCategoryProbability = new HashMap<>();
+        wordInCategoryProbability.put(CATEGORIES[0], americaWords);
+        wordInCategoryProbability.put(CATEGORIES[1], europeWords);
+
+        Bayes bayes = new Bayes();
+        Map<String, Map<String, Double>> actual = bayes.trainWordInCategoryProbability(texts);
+
+        Assert.assertEquals(wordInCategoryProbability.size(), actual.size());
+        for (String category : CATEGORIES) {
+            Assert.assertEquals(wordInCategoryProbability.get(category).size(), actual.get(category).size());
+            for (String word : WORDS) {
+                Assert.assertEquals(wordInCategoryProbability.get(category).get(word), actual.get(category).get(word), PRECISION);
+            }
+        }
+    }
+
+    @Test
+    public void testTest() {
+        Map<String, String[]> texts = createTrainTexts();
+        Bayes bayes = new Bayes();
+        bayes.train(texts);
+
+        Map<String, Double> probabilities = new HashMap<>();
+        Double americaProbability = (5.0 / 9.0) * (8.0 / (8.0 + WORDS.length)) * (8.0 / (8.0 + WORDS.length)) * (8.0 / (8.0 + WORDS.length))
+                * (6.0 / (6.0 + WORDS.length)) * (3.0 / (2.0 + WORDS.length)) * (1.0 / (4.0 + WORDS.length)) * (1.0 / (1.0 + WORDS.length))
+                * (1.0 / (2.0 + WORDS.length)) * (1.0 / WORDS.length);
+        probabilities.put(CATEGORIES[0], americaProbability);
+        Double europeProbability = (4.0 / 9.0) * (2.0 / (8.0 + WORDS.length)) * (2.0 / (8.0 + WORDS.length)) * (2.0 / (8.0 + WORDS.length))
+                * (2.0 / (6.0 + WORDS.length)) * (1.0 / (2.0 + WORDS.length)) * (5.0 / (4.0 + WORDS.length)) * (2.0 / (1.0 + WORDS.length))
+                * (3.0 / (2.0 + WORDS.length)) * (1.0 / WORDS.length);
+        probabilities.put(CATEGORIES[1], europeProbability);
+
+        Map<String, Double> actual = bayes.test(createTestText());
+
+        Assert.assertEquals(probabilities.size(), actual.size());
+        for (String category : CATEGORIES) {
+            Assert.assertEquals(probabilities.get(category), actual.get(category), PRECISION);
+        }
+    }
+
+    private Map<String, String[]> createTrainTexts() {
+        Map<String, String[]> texts = new HashMap<>();
+
+
+        String[] americaTexts = {
+                WORDS[0] + " " + WORDS[1] + " " + WORDS[0] + " " + WORDS[2],
+                WORDS[0] + " " + WORDS[1] + " " + WORDS[3] + " " + WORDS[4] + " " + WORDS[5],
+                WORDS[6] + " " + WORDS[0] + " " + WORDS[1] + " " + WORDS[7],
+                WORDS[8] + " " + WORDS[9] + " " + WORDS[0] + " " + WORDS[1] + " " + WORDS[0] + " " + WORDS[2],
+                WORDS[0] + " " + WORDS[1] + " " + WORDS[7]
+        };
+        String[] europeTexts = {
+                WORDS[1] + " " + WORDS[10] + " " + WORDS[11] + " " + WORDS[12],
+                WORDS[11] + " " + WORDS[0] + " " + WORDS[13] + " " + WORDS[11] + " " + WORDS[14],
+                WORDS[15] + " " + WORDS[16],
+                WORDS[11] + " " + WORDS[15] + " " + WORDS[17]
+        };
+
+        texts.put(CATEGORIES[0], americaTexts);
+        texts.put(CATEGORIES[1], europeTexts);
+
+        return texts;
+    }
+
+    private String createTestText() {
+        return WORDS[0] + " " + WORDS[1] + " " + WORDS[0] + " " + WORDS[2] + " " + WORDS[15] + " " + WORDS[11] + " "
+                + WORDS[0] + " " + WORDS[13] + " " + "Warsaw";
+    }
+}
